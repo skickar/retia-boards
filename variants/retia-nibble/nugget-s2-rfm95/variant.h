18a76e4c8bcd8d57c0f1472e91b04c45451e9d18
Whiskey26 <whiskey26@protonmail.com>
2025-12-26 15:11:59 -0800

    Add code for s2 nugget w/ rfm95 backpack

---
+++ variants/retia-nibble/nugget-s2-rfm95/variant.h
@@ -0,0 +1,27 @@
+#define I2C_SDA 33 // I2C pins for this board
+#define I2C_SCL 35
+
+#define USE_SH1106
+
+#define LED_PIN 15 // If defined we will blink this LED
+
+#define HAS_NEOPIXEL                         // Enable the use of neopixels
+#define NEOPIXEL_COUNT 2                     // How many neopixels are connected
+#define NEOPIXEL_DATA 12                     // gpio pin used to send data to the neopixels
+#define NEOPIXEL_TYPE (NEO_GRB + NEO_KHZ800) // type of neopixels in use
+
+#define ENABLE_AMBIENTLIGHTING
+
+#define BUTTON_PIN 7 // If defined, this will be used for user button presses
+#define BUTTON_NEED_PULLUP
+
+#define USE_RF95
+#define LORA_SCK 6
+#define LORA_MISO 8
+#define LORA_MOSI 10
+#define LORA_CS 13
+#define LORA_DIO0 16
+#define LORA_RESET 5
+
+#define LORA_DIO1 RADIOLIB_NC
+#define LORA_DIO2 RADIOLIB_NC
